6c4728c510f4629a59eb89c22c5ebdee74c6a2f3
Priyanka Sharma <rashika8025@gmail.com>
2026-02-24 23:38:26 +0530

    fix production api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-
+import axios from 'axios';
+// console.log(import.meta.env.VITE_REACT_APP_API_URL)
 const api = axios.create({
-  baseURL: "https://assignly-0h2y.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5000/api",
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 export default api;
