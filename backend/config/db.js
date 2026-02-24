7d71e541d9cb17f817580b26030cfde4ce0f6ed9
Priyanka Sharma <rashika8025@gmail.com>
2026-02-24 23:31:15 +0530

    fix api url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-// const MONGODB_URI=  process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/task-management';
+const MONGODB_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/task-management';
 
-const connectDb= (async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongoDb connected");        
+const connectDb = (async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log("mongoDb connected");
     }
-    catch(err){
+    catch (err) {
         console.log("connection error", err);
         process.exit(1);
     }
 });
 
 
-module.exports= connectDb;
+module.exports = connectDb;
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+// console.log(import.meta.env.VITE_REACT_APP_API_URL)
 const api = axios.create({
-    baseURL: "https://assignly-0h2y.onrender.com",
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5000/api",
     headers: {
         'Content-Type': 'application/json'
     }
